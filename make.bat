8f05072e4f09bc9540ac534be5dd3e6decc7de67
paaco <paaco@users.noreply.github.com>
2020-05-28 14:49:03 +0200

    Mini player frequency table, init and pulse mod

---
+++ make.bat
@@ -0,0 +1,2 @@
+ exomizer sfx systrim .cache\player1.prg -o player1p.prg
+ dir player1p.prg
--- player1.asm
+++ player1.asm
@@ -16,76 +16,71 @@ start:
             dec $D020
             jmp --
 
+
 ;-------------
 ; mini player
 ;-------------
 
     *= $1000
 
-!addr SID = $50     ; shadow copy of 3x7 SID registers in ZP
+!addr SID = $D400
 
-FREQL = 0
-FREQH = 1
-PWL = 2
-PWH = 3
-WAVE = 4
+FL = 0
+FH = 1
+PL = 2
+PH = 3
+WV = 4
 AD = 5
 SR = 6
+FILT_LO = $15
+FILT_HI = $16
+FILT_VOICES = $17
+FILT_VOL = $18
+
+; instrument data
+INS_P0 = 2 ; instrument pulse-width init
+INS_PM = 3 ; instrument pulse-width modulate
+INS_WV = 4
+INS_AD = 5
+INS_SR = 6
 
 music_init:
             clc
             bcc .music_init
 
 music_play:
-            ldx #13
--           lda SID,x
-            sta $D400,x
-.init:      dex
-            bpl -
+            ldx #0 ; voice offset (0,7,14)
 
-            lda note
-            asl         ; account for words
-            tay
+            ldy note ; 4 octaves of 7 notes (0..27)
+            lda music_freq_hi,y
+            sta SID+FH,x
+            lda music_freq_lo,y
+            sta SID+FL,x
 
-            ; ; octave up
-            ; lda freq_table,y
-            ; ;asl
-            ; sta SID+FREQL
-            ; lda freq_table+1,y
-            ; ;rol
-            ; sta SID+FREQH
+            ldy #$00 ; only use high-nibble
+            lda music_instruments+INS_AD,y
+            sta SID+AD,x
+            lda music_instruments+INS_SR,y
+            sta SID+SR,x
+            lda music_instruments+INS_WV,y
+            sta SID+WV,x
+            ;lda music_instruments+INS_PM,y
+            ;sta v1pulsemod,x
+            ;lda music_instruments+INS_P0,y
+            ;sta v1pulse,x
+            ;bne .set_pulsewidth
 
-            ; octave down
-            lda freq_table+1,y
-            ;lsr
-            sta SID+FREQH
-            lda freq_table,y
-            ;ror
-            sta SID+FREQL
-
-            ; double down
-            ; lda freq_table+1+14,y
-            ; sta SID+FREQH
-            ; lda freq_table+14,y
-            ; sta SID+FREQL
-            ; lsr SID+FREQH
-            ; ror SID+FREQL
-            ; lsr SID+FREQH
-            ; ror SID+FREQL
-
-            lda #$8C
-            sta SID+AD
-            lda #$44
-            sta SID+SR
-            lda #$41
-            sta SID+WAVE
-
-            inc pulse
-            lda pulse ; pulse-width
+.pulsemod:
+            ; pulse modulate
+            lda v1pulsemod,x
+            beq .no_pulsemod
+            clc
+            adc v1pulse,x
+            sta v1pulse,x
+.set_pulsewidth
             bpl +
             eor #$FF
-+           ; TODO pretty expensive way to flip nibbles
-            cmp #$80
++           cmp #$80
             rol
             cmp #$80
             rol
@@ -93,8 +88,10 @@ music_play:
             rol
             cmp #$80
             rol
-            sta SID+PWL
-            sta SID+PWH
+.no_pulsemod:
+            sta SID+PH,x
+            ora #$01 ; avoid silence
+            sta SID+PL,x
 
             dec delay
             bne +
@@ -102,63 +99,88 @@ music_play:
             sta delay
 
             inc note
-            inc note
             lda note
-            cmp #6
+            cmp #4*7-1
             bne +
             lda #0
             sta note
 +           rts
 
 .music_init:
-            lda #0
-            tax
--           sta SID,x
-            inx
-            cpx #$18
-            bcc -
-            beq +
-            ; TODO init song values
-            bne .init
-+           lda #$0F ; set volume
-            bne -
+            ldx #$18
+-           lda music_SID_init,x
+            sta SID,x
+            dex
+            bpl -
+            ; TODO reset song pointers
+            rts
+
+
+            !align 255,0,0 ; just to see how long each part gets
+; --------------
+;  dynamic data
+;---------------
+
+; voice data (3x7 bytes; could be in ZP)
+v1pulse:        !byte $3F
+v1pulsemod:     !byte $01
+                !byte 0,0,0,0,0
 
 delay:      !byte 6
 note:       !byte 0
-pulse:      !byte 0
 
+
+            !align 255,0,0 ; just to see how long each part gets
 ; -------------
 ;  music data
 ; -------------
 
-; 14-note octave 4 and 5 in key Cmin: C D Eb F G Ab Bb
-freq_table:
-            !word $1168 ; C-4
-            ;!word $1271 ; C#4
-            !word $138a ; D-4
-            !word $14b3 ; D#4 Eb4
-            ;!word $15ee ; E-4
-            !word $173c ; F-4
-            ;!word $189e ; F#4
-            !word $1a15 ; G-4
-            !word $1ba2 ; G#4 Ab4
-            ;!word $1d46 ; A-4
-            !word $1f04 ; A#4 Bb4
-            ;!word $20dc ; B#4
-            !word $22d0 ; C-5
-            ;!word $24e2 ; C#5
-            !word $2714 ; D-5
-            !word $2967 ; D#5 Eb5
-            ;!word $2bdd ; E-5
-            !word $2e79 ; F-5
-            ;!word $313c ; F#5
-            !word $3429 ; G-5
-            !word $3744 ; G#5 Ab5
-            ;!word $3a8d ; A-5
-            !word $3e08 ; A#5 Bb5
-            ;!word $41b8 ; B#5
-
 ; instruments ($10 bytes each)
-;                  AD  SR  PW
-music_data_instruments:
-            !byte $00,$00,$00, 0,0,0,0,0,0,0,0,0,0,0,0,0
+;              ------register init-------
+;              FL  FH  P0  PM  WV  AD  SR
+music_instruments:
+        !byte   0,  0,$3F,$F7,$41,$8C,$44, 0, 0, 0, 0, 0, 0, 0, 0, 0 ; pulse lead
+
+; TODO instrument wavetable
+;   TODO wavetable freq sweep (drum)
+;   TODO wavetable 81 FFFF step (burst)
+;   TODO wavetable wave
+;   TODO wavetable delay
+;   TODO wavetable end
+;   TODO wavetable hold-until-gateoff
+;   TODO wavetable set pulsemod
+;   TODO wavetable set pulsewidth
+;   TODO wavetable arp
+; TODO patterns
+;   TODO pattern start instrument at note
+;   TODO pattern gate-off
+;   TODO pattern delay
+;   TODO pattern set note (glissando)
+; TODO tracks
+; TODO song
+
+
+; 4 octaves of 7 notes octave 2,3,4 and 5 in key Cmin:
+;              0      1      2      3      4      5      6
+;              C      D      Eb     F      G      Ab     Bb
+music_freq_hi:
+        !byte >$045a,>$04e2,>$052d,>$05cf,>$0685,>$06e8,>$07c1 ; C-2
+        !byte >$08b4,>$09c5,>$0a5a,>$0b9e,>$0d0a,>$0dd1,>$0f82 ; C-3
+        !byte >$1168,>$138a,>$14b3,>$173c,>$1a15,>$1ba2,>$1f04 ; C-4
+        !byte >$22d0,>$2714,>$2967,>$2e79,>$3429,>$3744,>$3e08 ; C-5
+music_freq_lo:
+        !byte <$045a,<$04e2,<$052d,<$05cf,<$0685,<$06e8,<$07c1 ; C-2
+        !byte <$08b4,<$09c5,<$0a5a,<$0b9e,<$0d0a,<$0dd1,<$0f82 ; C-3
+        !byte <$1168,<$138a,<$14b3,<$173c,<$1a15,<$1ba2,<$1f04 ; C-4
+        !byte <$22d0,<$2714,<$2967,<$2e79,<$3429,<$3744,<$3e08 ; C-5
+
+; $18 bytes SID init
+music_SID_init:
+        !byte 0,0,0,0,0,0,0
+        !byte 0,0,0,0,0,0,0
+        !byte 0,0,0,0,0,0,0
+FILTER=0
+        !byte (FILTER & $F)     ; filter cutoff bits 3-0
+        !byte (FILTER >> 4)     ; filter cutoff bits 11-4
+        !byte $00               ;        reso | ext v3 v2 v1
+        !byte $0F               ; V3 HP BP LP | VOL
