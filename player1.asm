627c05ed070a3ccba1382f88606ccfa9597b6cf6
paaco <paaco@users.noreply.github.com>
2020-05-29 15:35:28 +0200

    Mini player track player

--- player1.asm
+++ player1.asm
@@ -14,6 +14,10 @@ start:
             inc $D020
             jsr $1003
             dec $D020
+            ; DEBUG make sure to wait at least a single rasterline
+            lda $d012
+-           cmp $d012
+            beq -
             jmp --
 
 
@@ -45,19 +49,18 @@ music_play:
             ldx #0                  ; voice offset (0,7,14)
             dec trackdelay
             bmi .do_tracks
-            bpl .do_wavetable       ; DEBUG remove this and uncomment below for 3 voices
-            ;jsr .do_wavetable
+            bpl .do_wavetable
             ;ldx #7                  ; voice offset (0,7,14)
             ;jsr .do_wavetable
     	    ;ldx #14                 ; voice offset (0,7,14)
             ;bne .do_wavetable
 
 .do_tracks:
-            lda #6                  ; TODO 5 for NTSC?
+            lda #6
             sta trackdelay
             ldy trackoff
             iny
-            cpy #$20
+            cpy #TRACK_LEN
             bmi +
             ldy #0
 +           sty trackoff
@@ -72,47 +75,34 @@ music_play:
             ldy trackoff
             lda music_tracks,y
             beq .do_wavetable       ; 0=do nothing
-            ; A=iiinnnnn
+            ; A=iiiinnnn
             pha
-            lsr
             and #$F0
-            tay                     ; Y=instrument offset ($00,$10,..,$70)
-            ; start instrument
+            tay                     ; Y=instrument offset ($10,$20..$F0)
+            pla
+            and #$0F                ; A=note offset (0..15)
+            clc
+            adc music_instruments+INS_RN,y  ; add instrument root note
+            sta v1note,x
+            ; restart instrument
+            lda music_instruments+INS_WT,y
+            sta v1waveidx,x
             lda music_instruments+INS_AD,y
             sta SID+AD,x
             lda music_instruments+INS_SR,y
             sta SID+SR,x
-            lda music_instruments+INS_P0,y
-            sta v1pulse,x
             lda music_instruments+INS_PM,y
             sta v1pulsemod,x
-            ; start wavetable
-            lda music_instruments+INS_WT,y
-            sta v1waveidx,x
+            lda music_instruments+INS_P0,y
+            sta v1pulse,x
+            beq +                   ; don't set pulsewidth if 0
+            jsr .set_pulsewidth
++           lda music_instruments+INS_FF,y
             tay
-            ; handle 81 FFFF burst
-            lda music_wavetable,y
-            cmp #$8F                ; 81 FFFF burst
             bne +
-            pla
-            and #$1F
-            sta v1note,x
-            lda #$81
-            sta SID+WV,x
-            and #$FE     ; DEBUG immediate gate-off
-            sta SID+WV,x ; DEBUG immediate gate-off
-            lda #$FF
-            sta SID+FL,x
-            sta SID+FH,x
-            sta v1freqh,x
-            rts
-+           sta SID+WV,x
-            and #$FE     ; DEBUG immediate gate-off
-            sta SID+WV,x ; DEBUG immediate gate-off
-            pla
-            and #$1F
-            tay                     ; Y=note offset 4 octaves of 7 notes (0..27)
-            lda music_freq_lo,y
+            ldy v1note,x
+            ; set note frequency
++           lda music_freq_lo,y
             sta SID+FL,x
             lda music_freq_hi,y
             sta SID+FH,x
@@ -120,26 +110,32 @@ music_play:
             ; fall-through
 
 .do_wavetable:
-            rts
+            ldy v1waveidx,x
+            lda music_wavetable,y
+            beq ++
++           sta SID+WV,x
+            inc v1waveidx,x
+++          rts
 
             ; TODO include code ---------------------------------------------
 
             ; freq sweep
             lda v1sweeph,x
-            beq .pulsemod
+            beq +
             clc
             adc v1freqh,x
             sta v1freqh,x
             sta SID+FH,x
-
-.pulsemod:
             ; pulse modulate
-            lda v1pulsemod,x
-            beq .no_pulsemod
++           lda v1pulsemod,x
+            beq +
             clc
             adc v1pulse,x
             sta v1pulse,x
-.set_pulsewidth
++
+            ; TODO /include code --------------------------------------------
+
+.set_pulsewidth:
             bpl +
             eor #$FF
 +           cmp #$80
@@ -150,12 +146,10 @@ music_play:
             rol
             cmp #$80
             rol
-.no_pulsemod:
             sta SID+PH,x
             ora #$01 ; avoid silence
             sta SID+PL,x
-
-            ; TODO /include code --------------------------------------------
+            rts
 
 
 .music_init:
@@ -172,29 +166,26 @@ music_play:
             rts
 
 
-            !align 255,0,0 ; just to see how long each part gets
 ; --------------
 ;  dynamic data
 ;---------------
 
 ; voice data (3x7 bytes; could be in ZP)
+v1note:         !byte 0
 v1waveidx:      !byte 0
-v1wavedelay:    !byte 0
 v1pulse:        !byte 0
 v1pulsemod:     !byte 0
 v1freqh:        !byte 0
 v1sweeph:       !byte 0
-v1note:         !byte 0
+                !fill 1,0
                 !fill 7,0 ; v2 copy
                 !fill 7,0 ; v3 copy
-
 
 ; player data (could be ZP)
 trackoff:       !byte 0
 trackdelay:     !byte 0
 
 
-            !align 255,0,0 ; just to see how long each part gets
 ; -------------
 ;  music data
 ; -------------
@@ -205,51 +196,67 @@ INS_SR = 1
 INS_WT = 2 ; wavetable offset
 INS_P0 = 3 ; instrument pulse-width init
 INS_PM = 4 ; instrument pulse-width modulate
+INS_RN = 5 ; root note
+INS_FF = 6 ; <>0 first frequency override use 28 for FFFF
 
 ; instruments ($10 bytes each)
-;               AD  SR  WVTBL    P0  PM
 music_instruments:
-        !byte  $02,$83, WVTBL0, $00,$00, 0,0,0,0,0,0,0,0,0,0,0 ; deep tom
-;       !byte  $25,$83, $81,    $00,$00, 0,0,0,0,0,0,0,0,0,0,0 ; snare
-;       !byte  $00,$60, $81,    $00,$00, 0,0,0,0,0,0,0,0,0,0,0 ; tick
-;       !byte  $8C,$44, $41,    $3F,$F7, 0,0,0,0,0,0,0,0,0,0,0 ; pulse lead
+        !fill 16,0 ; instrument 0 is not used
+;               AD  SR  WVTBL    P0  PM  RN FF
+        !byte  $02,$C3, WVTBL0, $00,$00,  7, 0, 0,0,0,0,0,0,0,0,0 ; deep tom
+;       !byte  $25,$83, $81,    $00,$00,  0, 0, 0,0,0,0,0,0,0,0,0 ; snare
+;       !byte  $00,$60, $81,    $00,$00,  0, 0, 0,0,0,0,0,0,0,0,0 ; tick
+;       !byte  $8C,$44, WVTBL1, $3F,$F7,  0, 0, 0,0,0,0,0,0,0,0,0 ; pulse lead
+        !byte  $52,$82, WVTBL1, $20,$F7,  14, 0, 0,0,0,0,0,0,0,0,0 ; pulse bass
 
 ; TODO instrument wavetable
 ;   TODO wavetable freq sweep (drum)
-;   TODO wavetable wave
+;   DONE wavetable wave
 ;   TODO wavetable delay
-;   TODO wavetable end
+;   DONE wavetable end
 ;   TODO wavetable hold-until-gateoff (.E?)
 ;   TODO wavetable set pulsemod
 ;   TODO wavetable set pulsewidth
 ;   TODO wavetable arp
 ; TODO tracks
-;   DONE track start instrument at note: A=iii_nnnnn
+;   DONE track start instrument at note: A=iiii_nnnn
 ;   TODO track gate-off:                 A=FF
-;   TODO track delay:                    A=00 do nothing
-;   TODO track set note (glissando):     A=111_nnnnn (E0..FF)
+;   DONE track delay:                    A=00 do nothing
+;   TODO track set note (legato):        A=1111_nnnn (E0..FF)
 ; TODO song
 
 
+TRACK_LEN = 32
 ;32 bytes per track (each byte represents 6 rasterlines)
 music_tracks:
         ; drum pattern
-        !byte $10,$00,$00,$14,$14,$00,$14,$00,$14,$00,$00,$00,$00,$00,$00,$00
-        !byte $10,$00,$00,$14,$14,$00,$14,$00,$14,$00,$00,$00,$14,$00,$00,$00
+        !byte $10,$00,$00,$14, $14,$00,$14,$00, $14,$00,$00,$00, $00,$00,$00,$00
+        !byte $20,$21,$22,$23, $24,$25,$26,$27, $20,$00,$00,$00, $00,$00,$00,$00
+        !byte $10,$00,$00,$14, $14,$00,$14,$00, $14,$00,$00,$00, $14,$00,$00,$00
 
 
-; wavetable is 1 byte per rasterline
+; wavetable is 1 byte per rasterline (max 256 bytes)
 music_wavetable:
         WVTBL0 = *-music_wavetable
-        !byte $8F ; $81 with $FFFF burst
-        !byte $11 ; resets actual note frequency because of $8F
-        !byte $AF ; freqh sweep depth 16 (already sweeps)
-        !byte 4   ; <16 is delay
+        !byte $81
+        !byte $11
+        ;!byte $AF ; freqh sweep depth 16 (already sweeps)
+        ;!byte 4   ; <16 is delay
+        !byte $11
+        !byte $11
+        !byte $11
+        !byte $11
         !byte $10 ; gate-off
         !byte $00 ; stop
         ;$90-$9F ARP
         ;$A0-$AF sweep down
         WVTBL1 = *-music_wavetable
+        !byte $41
+        !byte $41
+        !byte $41
+        !byte $41
+        !byte $41
+        !byte $40
         !byte $00 ; stop
 
 
@@ -261,13 +268,13 @@ music_freq_hi:
         !byte >$08b4,>$09c5,>$0a5a,>$0b9e,>$0d0a,>$0dd1,>$0f82 ; C-3
         !byte >$1168,>$138a,>$14b3,>$173c,>$1a15,>$1ba2,>$1f04 ; C-4
         !byte >$22d0,>$2714,>$2967,>$2e79,>$3429,>$3744,>$3e08 ; C-5
-        !byte $ff ; note 29 is max freq
+        !byte $ff ; note 28 is max freq
 music_freq_lo:
         !byte <$045a,<$04e2,<$052d,<$05cf,<$0685,<$06e8,<$07c1 ; C-2
         !byte <$08b4,<$09c5,<$0a5a,<$0b9e,<$0d0a,<$0dd1,<$0f82 ; C-3
         !byte <$1168,<$138a,<$14b3,<$173c,<$1a15,<$1ba2,<$1f04 ; C-4
         !byte <$22d0,<$2714,<$2967,<$2e79,<$3429,<$3744,<$3e08 ; C-5
-        !byte $ff ; note 29 is max freq
+        !byte $ff ; note 28 is max freq
 
 
 ; $18 bytes SID init
