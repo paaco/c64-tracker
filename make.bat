0721e29ce111910a3e07853e913ceea0205d0a0f
paaco <paaco@users.noreply.github.com>
2020-06-24 10:02:04 +0200

    Drum kit player basic song

--- make.bat
+++ make.bat
@@ -1,2 +1,3 @@
- exomizer sfx systrim .cache\player1.prg -o player1p.prg
- dir player1p.prg
+ exomizer sfx systrim .cache\player1.prg -o .cache\player1p.prg
+ exomizer sfx systrim .cache\player2.prg -o .cache\player2p.prg
+ dir .cache\player?p.prg
